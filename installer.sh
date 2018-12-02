5b70684d24983274a3d3022c0653bfe6b8504061
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-02 21:50:07 +0100

    [Installer] Added basic installer script

---
+++ installer.sh
@@ -0,0 +1,25 @@
+##########################################
+# Requirements:                          #
+# - colors.sh                            #
+##########################################
+
+STEP=0
+SUBSTEP=0
+
+function InstallStep {
+    if [[ ! $SUBSTEP -eq 0 ]]; then
+        SUBSTEP=0
+    fi
+
+	STEP=$((STEP + 1))
+	echo ""
+	echo "Step $STEP: ${CYAN}$@${RESET}"
+	echo ""
+}
+
+function InstallSubstep {
+    SUBSTEP=$((SUBSTEP + 1))
+    echo ""
+    echo "Step $STEP.$SUBSTEP: ${BLUE}$@${RESET}"
+    echo ""
+}
