8a258a53b8d3420ef386ba28d0a101cbfc9c30d5
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-02 23:09:18 +0100

    [Toggl] Added basic function for installing Toggl

---
+++ packages/toggl.sh
@@ -0,0 +1,34 @@
+##########################################
+# Requirements:                          #
+# - Commands:                            #
+#   - tempfile                           #
+# - Scripts                              #
+#   - installer.sh                       #
+#   - services/github.sh                 #
+##########################################
+
+function installToggl {
+    InstallSubstep Fetching latest Toggl release
+
+    latestRelease=$(getLatestGitHubReleaseUrlWithSpecificFileExtension "toggl/toggldesktop" ".tar.gz")
+    
+    InstallSubstep Downloading Toggl
+
+    togglFile=`tempfile`
+    curl --silent -L -o $togglFile $latestRelease
+
+    InstallSubstep Unpacking Toggl
+
+    tar xzf $togglFile
+
+    InstallSubstep Removing downloaded file
+
+    rm $togglFile
+
+    InstallSubstep Moving Toggl to the right directory
+
+    mv toggldesktop /opt/
+
+    InstallSubstep Installing Toggl
+    ln -s /opt/toggldesktop/TogglDesktop $HOME/Schreibtisch/Toggl
+}
